692c89390a53ef48c0c67bca39de17e8e3ed7aa7
santhi <santhipriyaveligarapu@gmail.comgit config --global user.name>
2026-05-20 11:58:23 +0530

    Fixed Spring Security

--- src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -3,39 +3,18 @@ package com.example.taskmanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    // PASSWORD ENCODER
-
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    // SECURITY
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
-
             .authorizeHttpRequests(auth -> auth
-
-                // ALLOW AUTH APIs
-                .requestMatchers("/api/auth/**").permitAll()
-
-                // ALLOW TASK APIs
-                .requestMatchers("/api/tasks/**").permitAll()
-
-                // EVERYTHING ELSE
-                .anyRequest().authenticated()
-
+                .anyRequest().permitAll()
             );
 
         return http.build();
