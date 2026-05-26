ad452e6ece9cdeeebfb0e925dea2576b2c7ad012
santhi <santhipriyaveligarapu@gmail.comgit config --global user.name>
2026-05-26 20:42:53 +0530

    Fixed security 403 issue

--- src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.example.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,27 +16,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+
                 .csrf(csrf -> csrf.disable())
 
                 .cors(cors -> {})
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                      "/api/auth/**",
-                              "/api/projects/**"
-                               ).permitAll()
-                        .anyRequest().authenticated()
+
+                        .anyRequest().permitAll()
+
                 )
 
                 .sessionManagement(session ->
+
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 );
 
         return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+}
     }
 }
